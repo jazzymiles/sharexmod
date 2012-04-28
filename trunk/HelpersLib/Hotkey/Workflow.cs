abafa3eaba3cd5fe2c1765cff326d28881ed11a8
mcored <mcored@37f6c2e1-dd6b-6eaa-9c45-039f80b438be>
2012-04-28 05:44:44 +0000

    menu items did not call DoWork

--- trunk/HelpersLib/Hotkey/Workflow.cs
+++ trunk/HelpersLib/Hotkey/Workflow.cs
@@ -10,10 +10,17 @@ namespace HelpersLib
     {
         public HotkeySetting HotkeyConfig = new HotkeySetting();
         public List<EActivity> Activities = new List<EActivity>();
+        public EHotkey Hotkey;
 
         public Workflow() { }
 
-        public Workflow(string description, HotkeySetting hotkeyConfig, bool bProtected = false)
+        public Workflow(EHotkey hotkey, HotkeySetting hotkeyConfig)
+            : this(hotkey.GetDescription(), hotkeyConfig, true)
+        {
+            this.Hotkey = hotkey;
+        }
+
+        public Workflow(string description, HotkeySetting hotkeyConfig, bool bProtected)
         {
             this.HotkeyConfig = hotkeyConfig;
             this.HotkeyConfig.SystemHotkey = bProtected;
