6ab66afe91eb8133b80abba124c8544254723f5c
mcored <mcored@37f6c2e1-dd6b-6eaa-9c45-039f80b438be>
2012-05-01 23:56:38 +0000

    Added ListViewAppender.cs and Viewer

--- trunk/ShareX/log4netHelpers/log4netHelper.cs
+++ trunk/ShareX/log4netHelpers/log4netHelper.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using HelpersLib;
 using log4net.Appender;
 using log4net.Config;
 using log4net.Core;
@@ -17,6 +18,7 @@ namespace ShareX
     {
         private static log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         private static RichTextBoxAppender rba;
+        private static ListViewAppender lva;
 
         public static void Init_log4net()
         {
@@ -33,6 +35,16 @@ namespace ShareX
                 log4net.Config.BasicConfigurator.Configure(fa);
                 fa.ActivateOptions();
             }
+        }
+
+        public static void ConfigureListView(MyListView myListView)
+        {
+            lva = new ListViewAppender(myListView);
+            lva.Threshold = Level.All;
+            lva.Layout = new PatternLayout("%date{ISO8601} [%thread] %-5level - [%logger] %m%n%exception");
+
+            BasicConfigurator.Configure(lva);
+            lva.ActivateOptions();
         }
 
         public static void ConfigureRichTextBox(RichTextBox myRichTextBox)
