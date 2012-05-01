5ffdb6b7c9f3632af2ce99280ca337afe3943e1d
mcored <mcored@37f6c2e1-dd6b-6eaa-9c45-039f80b438be>
2012-05-01 00:29:02 +0000

    added tree view based OptionsWindow.cs (partial commit)

---
+++ trunk/ShareX/Forms/OptionsWindow.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace ShareX.Forms
+{
+    partial class OptionsWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("General");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Shapes");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Capture", new System.Windows.Forms.TreeNode[] {
+            treeNode2});
+            this.tlpMain = new System.Windows.Forms.TableLayoutPanel();
+            this.tvMain = new System.Windows.Forms.TreeView();
+            this.panelBase = new System.Windows.Forms.Panel();
+            this.panelGeneral = new System.Windows.Forms.Panel();
+            this.cbCheckUpdates = new System.Windows.Forms.CheckBox();
+            this.cbStartWithWindows = new System.Windows.Forms.CheckBox();
+            this.cbShowTray = new System.Windows.Forms.CheckBox();
+            this.cbShellContextMenu = new System.Windows.Forms.CheckBox();
+            this.panelCapture = new System.Windows.Forms.Panel();
+            this.panelShapes = new System.Windows.Forms.Panel();
+            this.tlpMain.SuspendLayout();
+            this.panelBase.SuspendLayout();
+            this.panelGeneral.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tlpMain
+            // 
+            this.tlpMain.ColumnCount = 2;
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tlpMain.Controls.Add(this.tvMain, 0, 0);
+            this.tlpMain.Controls.Add(this.panelBase, 1, 0);
+            this.tlpMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpMain.Location = new System.Drawing.Point(0, 0);
+            this.tlpMain.Name = "tlpMain";
+            this.tlpMain.RowCount = 1;
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpMain.Size = new System.Drawing.Size(663, 554);
+            this.tlpMain.TabIndex = 0;
+            // 
+            // tvMain
+            // 
+            this.tvMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tvMain.Location = new System.Drawing.Point(3, 3);
+            this.tvMain.Name = "tvMain";
+            treeNode1.Name = "tnGeneral";
+            treeNode1.Tag = "panelGeneral";
+            treeNode1.Text = "General";
+            treeNode2.Name = "tnShapes";
+            treeNode2.Tag = "panelShapes";
+            treeNode2.Text = "Shapes";
+            treeNode3.Name = "tnCapture";
+            treeNode3.Tag = "panelCapture";
+            treeNode3.Text = "Capture";
+            this.tvMain.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode3});
+            this.tvMain.Size = new System.Drawing.Size(192, 548);
+            this.tvMain.TabIndex = 0;
+            // 
+            // panelBase
+            // 
+            this.panelBase.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panelBase.Controls.Add(this.panelGeneral);
+            this.panelBase.Controls.Add(this.panelCapture);
+            this.panelBase.Controls.Add(this.panelShapes);
+            this.panelBase.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelBase.Location = new System.Drawing.Point(201, 3);
+            this.panelBase.Name = "panelBase";
+            this.panelBase.Size = new System.Drawing.Size(459, 548);
+            this.panelBase.TabIndex = 1;
+            // 
+            // panelGeneral
+            // 
+            this.panelGeneral.BackColor = System.Drawing.SystemColors.Control;
+            this.panelGeneral.Controls.Add(this.cbCheckUpdates);
+            this.panelGeneral.Controls.Add(this.cbStartWithWindows);
+            this.panelGeneral.Controls.Add(this.cbShowTray);
+            this.panelGeneral.Controls.Add(this.cbShellContextMenu);
+            this.panelGeneral.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelGeneral.Location = new System.Drawing.Point(0, 0);
+            this.panelGeneral.Name = "panelGeneral";
+            this.panelGeneral.Size = new System.Drawing.Size(459, 548);
+            this.panelGeneral.TabIndex = 0;
+            // 
+            // cbCheckUpdates
+            // 
+            this.cbCheckUpdates.AutoSize = true;
+            this.cbCheckUpdates.Location = new System.Drawing.Point(16, 88);
+            this.cbCheckUpdates.Name = "cbCheckUpdates";
+            this.cbCheckUpdates.Size = new System.Drawing.Size(209, 17);
+            this.cbCheckUpdates.TabIndex = 3;
+            this.cbCheckUpdates.Text = "Automatically check updates at startup";
+            this.cbCheckUpdates.UseVisualStyleBackColor = true;
+            this.cbCheckUpdates.CheckedChanged += new System.EventHandler(this.cbCheckUpdates_CheckedChanged);
+            // 
+            // cbStartWithWindows
+            // 
+            this.cbStartWithWindows.AutoSize = true;
+            this.cbStartWithWindows.Location = new System.Drawing.Point(16, 40);
+            this.cbStartWithWindows.Name = "cbStartWithWindows";
+            this.cbStartWithWindows.Size = new System.Drawing.Size(155, 17);
+            this.cbStartWithWindows.TabIndex = 1;
+            this.cbStartWithWindows.Text = "Start ShareX with Windows";
+            this.cbStartWithWindows.UseVisualStyleBackColor = true;
+            this.cbStartWithWindows.CheckedChanged += new System.EventHandler(this.cbStartWithWindows_CheckedChanged);
+            // 
+            // cbShowTray
+            // 
+            this.cbShowTray.AutoSize = true;
+            this.cbShowTray.Location = new System.Drawing.Point(16, 16);
+            this.cbShowTray.Name = "cbShowTray";
+            this.cbShowTray.Size = new System.Drawing.Size(134, 17);
+            this.cbShowTray.TabIndex = 0;
+            this.cbShowTray.Text = "Show ShareX tray icon";
+            this.cbShowTray.UseVisualStyleBackColor = true;
+            this.cbShowTray.CheckedChanged += new System.EventHandler(this.cbShowTray_CheckedChanged);
+            // 
+            // cbShellContextMenu
+            // 
+            this.cbShellContextMenu.AutoSize = true;
+            this.cbShellContextMenu.Location = new System.Drawing.Point(16, 64);
+            this.cbShellContextMenu.Name = "cbShellContextMenu";
+            this.cbShellContextMenu.Size = new System.Drawing.Size(270, 17);
+            this.cbShellContextMenu.TabIndex = 2;
+            this.cbShellContextMenu.Text = "Show \"Upload using ShareX\" in Shell context menu";
+            this.cbShellContextMenu.UseVisualStyleBackColor = true;
+            this.cbShellContextMenu.CheckedChanged += new System.EventHandler(this.cbShellContextMenu_CheckedChanged);
+            // 
+            // panelCapture
+            // 
+            this.panelCapture.BackColor = System.Drawing.SystemColors.Control;
+            this.panelCapture.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelCapture.Location = new System.Drawing.Point(0, 0);
+            this.panelCapture.Name = "panelCapture";
+            this.panelCapture.Size = new System.Drawing.Size(459, 548);
+            this.panelCapture.TabIndex = 1;
+            // 
+            // panelShapes
+            // 
+            this.panelShapes.BackColor = System.Drawing.SystemColors.Control;
+            this.panelShapes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelShapes.Location = new System.Drawing.Point(0, 0);
+            this.panelShapes.Name = "panelShapes";
+            this.panelShapes.Size = new System.Drawing.Size(459, 548);
+            this.panelShapes.TabIndex = 2;
+            // 
+            // OptionsWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 554);
+            this.Controls.Add(this.tlpMain);
+            this.Name = "OptionsWindow";
+            this.Text = "OptionsWindow";
+            this.tlpMain.ResumeLayout(false);
+            this.panelBase.ResumeLayout(false);
+            this.panelGeneral.ResumeLayout(false);
+            this.panelGeneral.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlpMain;
+        private System.Windows.Forms.TreeView tvMain;
+        private System.Windows.Forms.Panel panelBase;
+        private System.Windows.Forms.Panel panelGeneral;
+        private System.Windows.Forms.Panel panelCapture;
+        private System.Windows.Forms.Panel panelShapes;
+        private System.Windows.Forms.CheckBox cbCheckUpdates;
+        private System.Windows.Forms.CheckBox cbStartWithWindows;
+        private System.Windows.Forms.CheckBox cbShowTray;
+        private System.Windows.Forms.CheckBox cbShellContextMenu;
+    }
+}
