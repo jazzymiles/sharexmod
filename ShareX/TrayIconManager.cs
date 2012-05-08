ace434766c6f08dac6e73cef321329e9f6024913
mcored <mcored@37f6c2e1-dd6b-6eaa-9c45-039f80b438be>
2012-05-08 05:27:46 +0000

    Icon rotates while ShareX is busy

---
+++ ShareX/TrayIconManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using ShareX.Properties;
+
+namespace ShareX
+{
+    public static class TrayIconManager
+    {
+        private static BackgroundWorker bwTray = new BackgroundWorker();
+
+        public static void UpdateTrayIcon()
+        {
+            if (!bwTray.IsBusy)
+            {
+                bwTray.DoWork += new DoWorkEventHandler(bwTray_DoWork);
+                bwTray.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bwTray_RunWorkerCompleted);
+                bwTray.RunWorkerAsync();
+            }
+        }
+
+        private static void RotateTrayIcon(int ms)
+        {
+            IEnumerable<Task> workingTasks = UploadManager.Tasks.Where(x => x != null && x.IsWorking);
+            while (workingTasks.Count() > 0)
+            {
+                System.Threading.Thread.Sleep(ms);
+                FormsHelper.Main.niTray.Icon = Resources.ShareXSmallBusyIcon1;
+                System.Threading.Thread.Sleep(ms);
+                FormsHelper.Main.niTray.Icon = Resources.ShareXSmallBusyIcon2;
+                System.Threading.Thread.Sleep(ms);
+                FormsHelper.Main.niTray.Icon = Resources.ShareXSmallBusyIcon3;
+                System.Threading.Thread.Sleep(ms);
+                FormsHelper.Main.niTray.Icon = Resources.ShareXSmallBusyIcon4;
+            }
+        }
+
+        private static void bwTray_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            FormsHelper.Main.niTray.Icon = Resources.ShareXSmallBusyIcon1;
+        }
+
+        private static void bwTray_DoWork(object sender, DoWorkEventArgs e)
+        {
+            RotateTrayIcon(500);
+        }
+    }
+}
