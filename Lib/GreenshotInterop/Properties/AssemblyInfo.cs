997b1b0267e2218df89081660e73ce8048e41ef0
mcored <mcored@37f6c2e1-dd6b-6eaa-9c45-039f80b438be>
2012-06-27 10:23:24 +0000

    AssemblyInfo.cs update

--- Lib/GreenshotInterop/Properties/AssemblyInfo.cs
+++ Lib/GreenshotInterop/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.200")]
+[assembly: AssemblyVersion("1.0.0.220")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
