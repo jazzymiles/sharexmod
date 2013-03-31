2b40d282334931a3a89ea6226e3ff7e3a6d9cbd5
mcored <mcored@37f6c2e1-dd6b-6eaa-9c45-039f80b438be>
2013-03-31 02:59:35 +0000

    Email address book can now be edited options form

--- ShareX/SettingsHelpers/UserConfig.cs
+++ ShareX/SettingsHelpers/UserConfig.cs
@@ -77,8 +77,10 @@ namespace ShareX.SettingsHelpers
             }
         }
 
-        // Email 
-        public List<string> AddressBook = new List<string>();
+        [Editor("System.Windows.Forms.Design.StringCollectionEditor, System.Design, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", typeof(System.Drawing.Design.UITypeEditor))]
+        [TypeConverter(typeof(CsvConverter))]
+        [Category(ComponentModelStrings.OutputsRemoteEmail), Description("Email address book")]
+        public List<string> AddressBook { get; set; }
 
         public UserConfig()
         {
