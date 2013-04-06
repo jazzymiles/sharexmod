8859adc20e1149c7bb278bbdc94c7b61306f17e3
mcored <mcored@37f6c2e1-dd6b-6eaa-9c45-039f80b438be>
2013-04-06 02:53:00 +0000

    Created tag version-7.2.2.

---
+++ tags/version-7.2.2/ShareX/HelperClasses/AddressBookHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShareX.HelperClasses
+{
+    public static class AddressBookHelper
+    {
+        public static string CurrentRecipient { get; set; }
+        private const int ADDRESSBOOK_LIMIT = 10;
+
+        /// <summary>
+        /// Add email to the Address Book and remove the last item if the count surpasses limit
+        /// </summary>
+        /// <param name="email">Email address</param>
+        /// <returns>True if added successfully</returns>
+        public static bool AddEmail(string email)
+        {
+            string result = SettingsManager.ConfigUser.AddressBook.SingleOrDefault(s => s == email);
+            if (string.IsNullOrEmpty(result))
+            {
+                SettingsManager.ConfigUser.AddressBook.Add(email);
+
+                if (SettingsManager.ConfigUser.AddressBook.Count > ADDRESSBOOK_LIMIT)
+                {
+                    SettingsManager.ConfigUser.AddressBook.RemoveAt(SettingsManager.ConfigUser.AddressBook.Count - 1);
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ tags/version-7.2.2/ShareX/SettingsHelpers/WorkflowsConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HelpersLib;
+using HelpersLib.Hotkeys2;
+
+namespace ShareX.SettingsHelpers
+{
+    public class WorkflowsConfig : SettingsBase<WorkflowsConfig>
+    {
+        public List<Workflow> Workflows = new List<Workflow>();
+
+        public void Add(Workflow wf)
+        {
+            Workflows.Add(wf);
+        }
+    }
+}
