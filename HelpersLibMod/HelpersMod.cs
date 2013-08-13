8ac15e4da04266d61cc3a82c326cd8d77efb9578
mcored <mcored@37f6c2e1-dd6b-6eaa-9c45-039f80b438be>
2013-08-13 10:49:29 +0000

    reimplemented https://code.google.com/p/sharexmod/source/detail?r=364 on the new ShareX base donate button is back for Berk

--- HelpersLibMod/HelpersMod.cs
+++ HelpersLibMod/HelpersMod.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.Win32;
+﻿using HelpersLib;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
 namespace HelpersLibMod
 {
@@ -22,5 +24,73 @@ namespace HelpersLibMod
         {
             return Image.FromStream(new MemoryStream(File.ReadAllBytes(fp)));
         }
+
+        public static bool ManageImageFolders(string rootDir, string patternSubFolder)
+        {
+            if (!string.IsNullOrEmpty(rootDir) && Directory.Exists(rootDir))
+            {
+                string[] images = Directory.GetFiles(rootDir);
+
+                List<string> imagesList = new List<string>();
+
+                List<string> listExt = new List<string>();
+                foreach (ImageFileExtensions ext in Enum.GetValues(typeof(ImageFileExtensions)))
+                {
+                    listExt.Add(ext.ToString());
+                }
+                foreach (VideoFileExtensions ext in Enum.GetValues(typeof(VideoFileExtensions)))
+                {
+                    listExt.Add(ext.ToString());
+                }
+
+                foreach (string image in images)
+                {
+                    foreach (string s in listExt)
+                    {
+                        if (Path.HasExtension(image) && Path.GetExtension(image.ToLower()) == "." + s)
+                        {
+                            imagesList.Add(image);
+                            break;
+                        }
+                    }
+                }
+
+                DebugHelper.WriteLine(string.Format("Found {0} images to move to sub-folders", imagesList.Count));
+
+                if (imagesList.Count > 0)
+                {
+                    if (MessageBox.Show(string.Format("{0} files found in {1}\n\nPlease wait until all the files are moved.",
+                        imagesList.Count, rootDir), Application.ProductName, MessageBoxButtons.OKCancel, MessageBoxIcon.Information) == DialogResult.Cancel)
+                    {
+                        return false;
+                    }
+
+                    DateTime time;
+                    string movePath;
+
+                    foreach (string image in imagesList)
+                    {
+                        if (File.Exists(image))
+                        {
+                            time = File.GetLastWriteTime(image);
+                            string subDirName = new NameParser(NameParserType.FolderPath) { CustomDate = time }.Parse(patternSubFolder);
+                            string subDirPath = Path.Combine(rootDir, subDirName);
+
+                            if (!Directory.Exists(subDirPath))
+                                Directory.CreateDirectory(subDirPath);
+
+                            movePath = Helpers.GetUniqueFilePath(subDirPath, Path.GetFileName(image));
+                            File.Move(image, movePath);
+                        }
+                    }
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+  
     }
 }
--- ShareX/Program.cs
+++ ShareX/Program.cs
@@ -211,7 +211,7 @@ namespace ShareX
         public const string URL_WEBSITE = "http://code.google.com/p/sharexmod";
         public const string URL_ISSUES = "http://code.google.com/p/sharexmod/issues/entry";
         public const string URL_UPDATE = "http://sharexmod.googlecode.com/svn/trunk/Update.xml";
-        public const string URL_DONATE = "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=mcored%40gmail%2ecom&lc=US&item_name=ShareXmod&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_SM%2egif%3aNonHostedGuest";
+        public const string URL_DONATE = "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=8DSJ2WNEHHDJJ&lc=US&item_name=ShareXmod&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted";
 
         public static List<string> LibNames = new List<string>();
 
